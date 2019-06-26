190d8880acd4773c7e143b71c8e5040ab61c2288
JakeTheGingScheuer <jacob.scheuer@accenture.com>
2019-06-26 10:08:40 -0400

    added a layout trial app

---
+++ basic_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('Hello World')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.deepOrange,
+            ),
+            child: Text('One')),
+          Container(
+            decoration: BoxDecoration(
+            color: Colors.green,
+            ),
+            child: Text('Two'))]
+          ),
+
+    )
+  ));
+}
