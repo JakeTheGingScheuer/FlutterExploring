bac9a958c80e70fa6c536792fccaef3c7ff7c530
JakeTheGingScheuer <jacob.scheuer@accenture.com>
2019-07-16 10:43:13 -0400

    day now has a balance and can add and delte transactions

--- test_driving/lib/day.dart
+++ test_driving/lib/day.dart
@@ -3,4 +3,18 @@ import 'package:test_driving/transaction.dart';
 
 class Day extends ChangeNotifier {
   List<Transaction> transactions = List<Transaction>();
+
+  double balance = 0.00;
+
+  double getBalance() {
+    return this.balance;
+  }
+
+  void addTransaction() {
+    transactions.add(Transaction());
+  }
+
+  void deleteTransaction(Transaction transaction) {
+    transactions.remove(transaction);
+  }
 }
--- test_driving/test/day_test.dart
+++ test_driving/test/day_test.dart
@@ -8,5 +8,25 @@ void main() {
 
       expect(testDay.transactions, []);
     });
+
+    test('Day should have a balance of 0.00 initally', () {
+      Day testDay = Day();
+
+      expect(testDay.getBalance(), 0.00);
+    });
+
+    test('Day should be able to add a new tranaction', () {
+      Day testDay = Day();
+      testDay.addTransaction();
+      expect(testDay.transactions.length, 1);
+    });
+
+    test('Day should be able to delete a transaction', () {
+      Day testDay = Day();
+      testDay.addTransaction();
+      testDay.deleteTransaction(testDay.transactions[0]);
+      expect(testDay.transactions.length, 0);
+    });
+
   });
 }
