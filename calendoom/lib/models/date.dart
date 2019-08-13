088bb431a59f0f97e27711c66b1a082ce9b745a9
JakeTheGingScheuer <jacob.scheuer@accenture.com>
2019-08-13 11:20:58 -0400

    date object should be good to go

--- calendoom/lib/models/date.dart
+++ calendoom/lib/models/date.dart
@@ -1,7 +1,27 @@
+import 'package:date_util/date_util.dart';
+
 class Date {
+  DateUtil dateUtil = DateUtil();
+  static const OFFSET = 2;
+
   int monthNumber;
   int dayNumber;
   int year;
+  int weekdayNumber;
+  String weekday;
+  String monthName;
 
-  Date(this.monthNumber, this.dayNumber, this.year);
+  Date(DateTime dateTime){
+    DateUtil dateUtil = DateUtil();
+    this.monthNumber = dateTime.month;
+    this.dayNumber = dateTime.day;
+    this.year = dateTime.year;
+    this.weekdayNumber = dateTime.weekday;
+    this.weekday = dateUtil.day(this.weekdayNumber+OFFSET);
+    this.monthName = dateUtil.month(this.monthNumber);
+  }
+
+  getTitle() {
+    return weekday+' '+monthName+' '+dayNumber.toString()+', '+year.toString();
+  }
 }
--- calendoom/test/unitTests/date_test.dart
+++ calendoom/test/unitTests/date_test.dart
@@ -4,10 +4,30 @@ import 'package:test_driving/models/date.dart';
 void main() {
   group('unit tests for date', () {
     test('Date initialization', () {
-      Date date = new Date(8,13,2019);
+      DateTime dateTime = new DateTime(2019, 8, 13);
+      Date date = new Date(dateTime);
       expect(date.monthNumber, 8);
       expect(date.dayNumber, 13);
       expect(date.year, 2019);
+    });
+
+    test('Date finds and stores its weekday number and string interpretation of it', () {
+      DateTime dateTime = new DateTime(2019, 8, 13);
+      Date date = new Date(dateTime);
+      expect(date.weekdayNumber, 2);
+      expect(date.weekday, 'Tuesday');
+    });
+
+    test('Date makes a String interpretation of the month', () {
+      DateTime dateTime = new DateTime(2019, 8, 13);
+      Date date = new Date(dateTime);
+      expect(date.monthName, 'August');
+    });
+
+    test('Get title returns a string of the full date', () {
+      DateTime dateTime = new DateTime(2019, 8, 13);
+      Date date = new Date(dateTime);
+      expect(date.getTitle(), 'Tuesday August 13, 2019');
     });
   });
 }
